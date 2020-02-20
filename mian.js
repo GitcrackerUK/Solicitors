6ebf872c3d59d96523383128443f9ef40ee411a6
gitcrackerUK <pfsiwek@gmail.com>
2020-02-20 22:26:01 +0000

    improved service buttons with vue v-on..

---
+++ mian.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        result: 'First',
+        show: false,
+        show1: false,
+        show2: false,
+        show3: false,
+        show4: false,
+        show5: false,
+        show6: false,
+        show7: false
+    },
+    methods: {
+
+    }
+});
